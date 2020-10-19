28e88e966510fae23d1e9c4f24eec2c15b17d38d
Bulat Yusupov <usbulat@gmail.com>
2020-10-19 16:59:26 +0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,20 @@
+from datetime import timedelta, datetime
+
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+
+from commons.datasources import sql_server_ds
+
+dag = DAG('orders',
+          schedule_interval=timedelta(hours=6),
+          start_date=datetime(2020, 7, 8, 0))
+
+def workflow(**context):
+    print(context)
+
+for conn_id, schema in sql_server_ds:
+    PythonOperator(
+        task_id=schema,
+        python_callable=workflow,
+        provide_context=True,
+        dag=dag)
